5d6c47a076b88b18be6e4feee2b8ba0bb048d870
Joonas Griesinger <griesinger.joonas@gmail.com>
2022-08-18 02:42:35 +0300

    Adding HtmlTargetElement attributes explicitly to our override. This fixes a bug where buttons with the re-invisible attribute where not rendered.

--- src/TagHelpers/RecaptchaInvisibleTagHelper.cs
+++ src/TagHelpers/RecaptchaInvisibleTagHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Griesoft.AspNetCore.ReCaptcha.Configuration;
 using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.Extensions.Options;
 
 namespace Griesoft.OrchardCore.ReCaptcha.TagHelpers
@@ -9,6 +10,10 @@ namespace Griesoft.OrchardCore.ReCaptcha.TagHelpers
     /// This tag helper only exists to override the namespace of the tag. To prevent name clashes by providing all
     /// tag helpers under the same namespace when importing them.
     /// </remarks>
+    [HtmlTargetElement("recaptcha-invisible", Attributes = "callback", TagStructure = TagStructure.WithoutEndTag)]
+    [HtmlTargetElement("recaptcha-invisible", Attributes = "form-id", TagStructure = TagStructure.WithoutEndTag)]
+    [HtmlTargetElement("button", Attributes = "re-invisible,callback")]
+    [HtmlTargetElement("button", Attributes = "re-invisible,form-id")]
     public class RecaptchaInvisibleTagHelper : AspNetCore.ReCaptcha.TagHelpers.RecaptchaInvisibleTagHelper
     {
         /// <inheritdoc/>
